c2c0c74abd0994bed032b78e8872eda4c07cfb7e
Andrey Belonog <andrey.belonog@skarb.ngo>
2018-07-05 10:13:40 +0300

    Refactored controller's URL

--- src/main/java/home/andreybelonog/controllers/BookController.java
+++ src/main/java/home/andreybelonog/controllers/BookController.java
@@ -13,7 +13,7 @@ public class BookController {
     private static final String TEMPLATE = "Here you will see: %s";
     private static AtomicLong counter = new AtomicLong();
 
-    @GetMapping("api/get-book")
+    @GetMapping("api/book")
     public Book getBook(@RequestParam(value = "name", defaultValue = "DefaultBook") String name){
         return new Book(counter.incrementAndGet(),
                         String.format(TEMPLATE, name));
