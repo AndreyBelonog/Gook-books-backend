20124cb4ac2e8258e7714b5f96a8e5b870c1da83
Andrey Belonog <andrey.belonog@skarb.ngo>
2018-07-05 09:27:08 +0300

    Added Book class

---
+++ src/main/java/home/andreybelonog/Book.java
@@ -0,0 +1,71 @@
+package home.andreybelonog;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private String authorFirstName;
+    private String authorSecondName;
+    private StringBuilder description;
+
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Book(int id, String title, String authorFirstName, String authorSecondName, StringBuilder description) {
+        this.id = id;
+        this.title = title;
+        this.authorFirstName = authorFirstName;
+        this.authorSecondName = authorSecondName;
+        this.description = description;
+    }
+
+    public Book(int id, String title, String authorFirstName, String authorSecondName) {
+        this.id = id;
+        this.title = title;
+        this.authorFirstName = authorFirstName;
+        this.authorSecondName = authorSecondName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorSecondName() {
+        return authorSecondName;
+    }
+
+    public void setAuthorSecondName(String authorSecondName) {
+        this.authorSecondName = authorSecondName;
+    }
+
+    public StringBuilder getDescription() {
+        return description;
+    }
+
+    public void setDescription(StringBuilder description) {
+        this.description = description;
+    }
+}
