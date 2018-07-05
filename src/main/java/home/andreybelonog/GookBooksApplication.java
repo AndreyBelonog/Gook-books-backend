d194965dca77456901f36a74701968ed9dd02334
Andrey Belonog <andrey.belonog@skarb.ngo>
2018-07-05 09:58:11 +0300

    Created 'GookBooksApplication' class with method 'main'

---
+++ src/main/java/home/andreybelonog/GookBooksApplication.java
@@ -0,0 +1,12 @@
+package home.andreybelonog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GookBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GookBooksApplication.class, args);
+    }
+}
