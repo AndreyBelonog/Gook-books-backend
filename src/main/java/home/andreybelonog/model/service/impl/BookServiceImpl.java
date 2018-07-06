8174f68e8eb4818b91df8720a66aecef1dcdf8f1
Andrey Belonog <andrey.belonog@skarb.ngo>
2018-07-06 22:40:37 +0300

    Created template methods and added nessecary annotations at BookServiceImpl

--- src/main/java/home/andreybelonog/model/service/impl/BookServiceImpl.java
+++ src/main/java/home/andreybelonog/model/service/impl/BookServiceImpl.java
@@ -1,4 +1,46 @@
 package home.andreybelonog.model.service.impl;
 
-public class BookServiceImpl {
+import home.andreybelonog.model.entity.Book;
+import home.andreybelonog.model.repository.BookRepository;
+import home.andreybelonog.model.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public void deleteBook(long id) {
+
+    }
+
+    @Override
+    public Book getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Book updateBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return null;
+    }
 }
