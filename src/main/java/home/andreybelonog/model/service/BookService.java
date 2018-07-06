dc2b2c071731760e0ce34e29f7e9a66b44b0bc47
Andrey Belonog <andrey.belonog@skarb.ngo>
2018-07-06 22:36:50 +0300

    Described minimal bussines logic into BookService

--- src/main/java/home/andreybelonog/model/service/BookService.java
+++ src/main/java/home/andreybelonog/model/service/BookService.java
@@ -1,4 +1,17 @@
 package home.andreybelonog.model.service;
 
+import home.andreybelonog.model.entity.Book;
+
+import java.util.List;
+
 public interface BookService {
+    Book addBook(Book book);
+
+    void deleteBook(long id);
+
+    Book getByName(String name);
+
+    Book updateBook(Book book);
+
+    List<Book> getAll();
 }
