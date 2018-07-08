619e90490c8ca10c44421a396fe66b302b8a2426
Andrey Belonog <andrey.belonog@skarb.ngo>
2018-07-08 14:04:41 +0300

    Removed unnecessary import

--- src/main/java/home/andreybelonog/controllers/BookController.java
+++ src/main/java/home/andreybelonog/controllers/BookController.java
@@ -5,7 +5,6 @@ import home.andreybelonog.model.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 
 @RestController
