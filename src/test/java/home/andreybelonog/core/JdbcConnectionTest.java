ad5ecf0a66cd99d868fce0aa295fbd0b65165476
Andrey Belonog <belonogandrey@gmail.com>
2018-07-10 20:03:09 +0300

    Added properties to 'JdbcConnectionTest' class

--- src/test/java/home/andreybelonog/core/JdbcConnectionTest.java
+++ src/test/java/home/andreybelonog/core/JdbcConnectionTest.java
@@ -1,4 +1,24 @@
 package home.andreybelonog.core;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class JdbcConnectionTest {
+    private String url;
+
+    private String username;
+
+    private String password;
+
+    private String validationQuery;
+
+    @Before
+    public void seyUp(){
+        url = "jdbc:postgresql://localhost/GookBookDB";
+        username = "postgres";
+        password = "pass";
+        validationQuery = "SELECT 1";
+    }
 }
