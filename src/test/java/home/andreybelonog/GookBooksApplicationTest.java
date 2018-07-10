39b3a7b93fe1db2e3c46a7cffc3f7513e381b27e
Andrey Belonog <belonogandrey@gmail.com>
2018-07-10 18:22:20 +0300

    Implemented general test class for application

--- src/test/java/home/andreybelonog/GookBooksApplicationTest.java
+++ src/test/java/home/andreybelonog/GookBooksApplicationTest.java
@@ -1,4 +1,16 @@
 package home.andreybelonog;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GookBooksApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GookBooksApplicationTest {
+
+    @Test
+    public void whenApplicationsMainMethodInvokedThenApplicationStartsSuccessfully(){
+        GookBooksApplication.main(new String[]{});
+    }
 }
